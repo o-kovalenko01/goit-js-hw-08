e4972eaae90bdce80168c16038130efb6b90f15e
o-kovalenko01 <kovalenkoleg777@gmail.com>
2023-10-19 18:51:30 +0300

    complelted first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryElement = document.querySelector('.gallery');
+
+function createGalleryItem(item) {
+  return `<li class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img class="gallery__image" src="${item.preview}"  alt="${item.description}" />
+            </a>
+        </li>`;
+}
+
+galleryElement.innerHTML = galleryItems
+  .map(item => createGalleryItem(item))
+  .join('');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
