3c28c89969f4c4e9e8b803e214c1a97dcd1d1ab0
o-kovalenko01 <kovalenkoleg777@gmail.com>
2023-10-22 19:59:31 +0300

    complelted second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const vimeoPlayer = new Player('vimeo-player', {});
+
+function onTimeUpdate(event) {
+  const currentTime = event.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}
+
+const throttledTimeUpdate = throttle(onTimeUpdate, 1000);
+
+vimeoPlayer.on('timeupdate', throttledTimeUpdate);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+  vimeoPlayer.setCurrentTime(parseFloat(savedTime));
+}
